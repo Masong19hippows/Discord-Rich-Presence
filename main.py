bfa537bbd941ca4ee5b1543ea7c593a4bc1880ae
Masong19hippows <garten323@gmail.com>
2020-10-21 14:08:28 -0500

    Working

--- main.py
+++ main.py
@@ -14,27 +14,29 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-  guild = discord.utils.get(client.guilds, name=GUILD)
-  member = discord.utils.get(guild.members, name="Masong19hippows")
-  for activity in member.activities:
-    if isinstance(activity, Game):
-       name = activity.name
-       Type = "Playing"
-       Activity = f"{name}"
-       print(Activity)
-    elif isinstance(activity, Streaming):
-       name2 = activity.name
-       name3 = activity.platform
-       Type = "Streaming"
-       Activity = f"{name2} on {name3}"
-       print(Activity)
-    elif isinstance(activity, Spotify):
-       name4 = activity.title
-       name5 = activity.artists
-       Type = "Listening to Spotify"
-       Activity = f"**Song Name**:{name4}\n**Song Artists:**{name5}"
-       print(Activity)
-    else:
-       print('none')
+   
+   guild = discord.utils.get(client.guilds, name=GUILD)
+   member = discord.utils.get(guild.members, name="Masong19hippows")
+   if member.activities == ():
+      print('none')
+   else:
+      for activity in member.activities:
+         if isinstance(activity, discord.activity.Game):
+            name = activity.name
+            Type = "Playing"
+            Activity = f"{name}"
+            print(Activity)
+         elif isinstance(activity, discord.activity.Streaming):
+            name2 = activity.name
+            name3 = activity.platform
+            Type = "Streaming"
+            Activity = f"{name2} on {name3}"
+            print(Activity)
+         elif isinstance(activity, discord.activity.Spotify):
+            name4 = activity.title
+            name5 = ', '.join(activity.artists)
+            Type = "Listening to Spotify"
+            Activity = f"Song Name: {name4}\nSong Artists: {name5}"
+            print(Activity)
 
 client.run(TOKEN)
